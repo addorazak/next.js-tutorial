4dee9309cacb822f2c692fca7eeff3f092e0a536
Addo Abdul-Razak <addorazak8@gmail.com>
2026-03-30 09:31:38 +0000

    added nested dynamic routes

---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,15 @@
+export default async function ReviewDetailPage({
+  params,
+}: {
+  params: Promise<{ productId: string; reviewId: string }>;
+}) {
+  const { productId, reviewId } = await params;
+  return (
+    <>
+      <h1>Review Detail</h1>
+      <p>
+        Review {reviewId} for product {productId}
+      </p>
+    </>
+  );
+}
