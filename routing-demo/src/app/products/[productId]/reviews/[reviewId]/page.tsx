26d589074e425c34504118c06a432e7e60b7a644
Addo Abdul-Razak <addorazak8@gmail.com>
2026-03-30 10:45:41 +0000

    added not-found.tsx page

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default async function ReviewDetailPage({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
   return (
     <>
       <h1>Review Detail</h1>
