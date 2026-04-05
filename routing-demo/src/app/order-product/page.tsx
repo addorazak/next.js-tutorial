f1cc228b3397dfe81a01e934da789b0339fa2989
Addo Abdul-Razak <addorazak8@gmail.com>
2026-04-05 18:52:46 +0000

    added navigating programmatically - useRouter & redirect

---
+++ routing-demo/src/app/order-product/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function OrderProduct() {
+  const router = useRouter();
+
+  const handleOrder = () => {
+    console.log("Placing order...");
+    router.push("/");
+  };
+  return (
+    <>
+      <h1>Order Product</h1>
+      <button onClick={handleOrder}>Place Order</button>
+    </>
+  );
+}
